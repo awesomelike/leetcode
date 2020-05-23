6889068ba7510095ec2fc2f8f3b4a2c97276be86
unknown <mr.azik98@mail.ru>
2020-05-23 22:41:39 +0500

    21++

---
+++ easy/21.cpp
@@ -0,0 +1,35 @@
+/*
+  Link: https://leetcode.com/problems/merge-two-sorted-lists/
+*/
+
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if (l1 == nullptr && l2 == nullptr) return nullptr;
+    else if (l1 == nullptr) return l2;
+    else if (l2 == nullptr) return l1;
+
+    ListNode *result = new ListNode();  
+
+    if (l1->val <= l2-> val) {
+      result = l1;
+      result->next = mergeTwoLists(l1->next, l2);
+    } else {
+      result = l2;
+      result->next = mergeTwoLists(l1, l2->next);
+    }
+    return result;
+  }
+};
