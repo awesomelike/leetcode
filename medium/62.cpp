a1ec24f9253017886f9ef457f5b40fde167359e9
awesomelike <abdullaev.dev@gmail.com>
2020-08-09 14:51:35 +0500

    62+

---
+++ medium/62.cpp
@@ -0,0 +1,40 @@
+/*
+  Link: https://leetcode.com/problems/unique-paths/
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+  int uniquePaths(int m, int n) {
+    int dp[n][m];
+    
+    for (int i=0; i<m; i++) {
+      dp[0][i] = 1;  
+    }
+    for (int i=0; i<n; i++) {
+      dp[i][0] = 1;  
+    }
+
+    for (int i=1; i < n; i++) {
+      for (int j=1; j < m; j++) {
+        dp[i][j] = dp[i-1][j] + dp[i][j-1];
+      }
+    }
+    return dp[n-1][m-1]; 
+  }
+};
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    cout << "Invalid input" << endl;
+    return -1;
+  }
+  int m = atoi(argv[1]), n = atoi(argv[2]);
+  Solution s;
+
+  cout << s.uniquePaths(m, n) << endl;
+  return 0;
+}
