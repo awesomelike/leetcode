0d22c0048c250b9bd939796c6b0cbaa011ff5121
awesomelike <abdullaev.dev@gmail.com>
2020-08-10 12:58:09 +0500

    713+

---
+++ medium/713.cpp
@@ -0,0 +1,45 @@
+/*
+  Link: https://leetcode.com/problems/subarray-product-less-than-k/
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+  int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+    int n = nums.size();
+    int p = 1;
+
+    int l = 0;
+    int r = 0;
+    
+    int count = 0;
+    
+    while (r < n)
+    {
+      p *= nums[r];
+      while (p >= k)
+      {
+        p/=nums[l];
+        l++;
+      }
+      
+      count += r-l+1;
+      r++;
+    }
+
+    return count;
+    
+  }
+};
+
+int main() {
+  vector<int> v = {10, 5, 2, 6};
+
+  Solution solution;
+
+  cout << solution.numSubarrayProductLessThanK(v, 100) << endl;
+  return 0;
+}
