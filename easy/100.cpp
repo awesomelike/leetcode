3c4f4f0aed8b225e286bccad2d494276a10357aa
awesomelike <abdullaev.dev@gmail.com>
2020-10-03 17:01:11 +0500

    optimized same tree logc

--- easy/100.cpp
+++ easy/100.cpp
@@ -16,10 +16,9 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-      if (p == nullptr && q == nullptr) return true;
-      else if (p != nullptr && q == nullptr) return false;
-      else if (p == nullptr && q != nullptr) return false;
-      
+      if (p == nullptr || q == nullptr) {
+        return p == q;
+      }
       if (p ->val != q->val) return false;
       
       return isSameTree(p->left, q->left) && isSameTree(p->right, q->right); 
