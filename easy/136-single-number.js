7c6f9dc1670547a9833533b72bb34760b1fc8ca8
awesomelike <abdullaev.dev@gmail.com>
2021-03-16 06:58:07 +0500

    refactor 136

--- easy/136-single-number.js
+++ easy/136-single-number.js
@@ -3,9 +3,5 @@
  * @return {number}
  */
  var singleNumber = function(nums) {
-  let res = nums[0];
-  for (let i=1; i<nums.length; i++) {
-    res = res ^ nums[i];
-  }
-  return res;
+  return nums.reduce((acc, cur) => acc ^ cur, []);
 };
