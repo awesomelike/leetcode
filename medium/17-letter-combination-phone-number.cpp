e8089d95a552c03e2284563b8f144c1859a041d4
awesomelike <abdullaev.dev@gmail.com>
2021-03-20 16:33:06 +0500

    add 17

--- medium/17-letter-combination-phone-number.cpp
+++ medium/17-letter-combination-phone-number.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-  vector<string> abc {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-  void dp(int i,string dig,string curr,vector<string>& ans)
+  vector<string> hash {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+  void dp(int i, string dig, string curr, vector<string>& ans)
   {
-    if(i==dig.size())
+    if(i == dig.size())
       return ans.push_back(curr);
-    int j = dig[i]-'0';
-    for(int a = 0;a < abc[j-2].size(); a++)
-      dp(i+1, dig, curr + abc[j-2][a], ans);
+    int digit = dig[i]-'0';
+    for(int k = 0; k < hash[digit-2].size(); k++)
+      dp(i+1, dig, curr + hash[digit-2][k], ans);
   }
   vector<string> letterCombinations(string digits) {
     vector<string> ans;
-    if(digits.size()==0) return ans;
-    dp(0,digits,"",ans);
+    if(digits.size() == 0) return ans;
+    dp(0, digits, "", ans);
     return ans;
   }
 };
