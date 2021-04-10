dbce9a8e9bc6a63701249de35e9e5170264458e6
awesomelike <abdullaev.dev@gmail.com>
2021-04-10 19:29:01 +0500

    add 566

---
+++ easy/566-reshape-matrix.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} nums
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+ var matrixReshape = function(nums, r, c) {
+  const m = nums.length;
+  const n = nums[0].length;
+  
+  if (m * n < r * c) {
+    return nums;
+  }
+  const temp = [];
+  for (a of nums) {
+    for (e of a) {
+      temp.push(e);
+    }
+  }
+
+  const ans = [];
+  for (let i=0; i<r; i++) {
+    ans.push([]);
+  }
+  
+  for (let i=0; i<r; i++) {
+    for (let j=0; j<c; j++) {
+      ans[i][j] = temp[i * c + j];
+    }
+  }
+  
+  return ans;
+};
