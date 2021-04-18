7b9e6a16c91dbf9fed97674f3fe855896d046bc4
awesomelike <abdullaev.dev@gmail.com>
2021-04-18 16:03:43 +0500

    add 141

---
+++ easy/141-linked-list-cycle.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var hasCycle = function(head) {
+  let t = head;
+  while (t !== null) {
+    t.visited = t.visited || 0;
+    t.visited++;
+    if (t.visited > 1 && t.next) {
+      return true;
+    }
+    t = t.next;
+  }
+  return false;
+};
